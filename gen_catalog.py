425823c780c757dd7addfa36c6746f3196b136b8
Fahmy-Kadiri-akl <94751930+Fahmy-Kadiri-akl@users.noreply.github.com>
2026-09-15 21:09:49 -0400

    Regenerate catalogue without harut-marut-babylon

--- gen_catalog.py
+++ gen_catalog.py
@@ -79,7 +79,7 @@ DOMAIN_MAP = {
               "text-destruction", "word-and-command"],
     "history": ["ali-forbearance", "umm-kulthum-marriage", "ifk-maria",
                "abu-bakr-strikes-aisha"],
-    "imams": ["light-of-prophet", "first-and-last", "majlis-al-rida", "tear-and-pool", "greatest-name", "harut-marut-babylon"],
+    "imams": ["light-of-prophet", "first-and-last", "majlis-al-rida", "tear-and-pool", "greatest-name"],
     "battles": ["battles-of-the-prophet"] + battle_order(),
     "reference": ["facts"],
 }
